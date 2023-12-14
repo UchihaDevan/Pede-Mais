d80e0fdef93a1f4a71400452ad805a09cf588571
KanashiShonen <45114712+UchihaDevan@users.noreply.github.com>
2023-12-13 23:20:21 -0300

    Add files via upload

---
+++ pedemais/src/main/java/com/pedemais/estoque.java
@@ -0,0 +1,29 @@
+package com.pedemais;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+class Main {
+
+    public static void main(String[] args) {
+        
+        ArrayList<User> usuarios = new ArrayList<User>();
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual o seu nome? ");
+        
+        System.out.println("Digite 5 sabores de pizza em sequência: ");
+        String produto1 = scanner.next();
+        String produto2 = scanner.next();
+        String produto3 = scanner.next();
+        String produto4 = scanner.next();
+        String produto5 = scanner.next();
+
+        User
+        
+        //System.out.println("Os produtos são: "+produto1+", "+produto2+", "+produto3+", "+produto4+" "+produto5);
+
+        scanner.close();
+    }
+    
+}
